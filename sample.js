d5a8901281972bf5d274febcacfc78652c3cde91
Tsuno Yuya <2ch.yuya@gmail.com>
2020-04-30 08:37:37 +0900

    要件と+α要件

--- sample.js
+++ sample.js
@@ -6,7 +6,7 @@ new Vue({
     name: 'A山B郎',
     course: 'Webエンジニアコース',
     acceptancePeriod: '2019年01月期',
-    defaultLastId: 3,
+    defaultLastId: 4,
     students: [
       { id: 1, name: '野呂浩良', course: '機械学習コース', acceptancePeriod: '2019年01月期' },
       { id: 2, name: '富永修司', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' },
@@ -18,6 +18,10 @@ new Vue({
     addStudent: function() {
         // この function() { } の中にaddStudentメソッド
         // （入力欄に入力された値と、ID値（最後に登録されたID値+1の値）が新たに出現する機能）の処理を書く
-    }
+        this.students.push({id: this.defaultLastId++, name: this.name, course: this.course, acceptancePeriod: this.acceptancePeriod });
+        this.name = '';
+        this.course = '';
+        this.acceptancePeriod = '';
+      }
   }
 })
